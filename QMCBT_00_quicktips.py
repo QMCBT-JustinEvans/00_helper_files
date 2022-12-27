057968e6c299602ae12cba88ca206c8a1b7ffe2b
QMCBT-JustinEvans <Justin.Ellis.Evans@gmail.com>
2022-12-26 23:31:14 -0600

    UPDATE helper modules

--- QMCBT_00_quicktips.py
+++ QMCBT_00_quicktips.py
@@ -1,34 +1,122 @@
-######################### Display Imports #########################
+#########################################################
+#################### Display Imports ####################
+#########################################################
 
 def imports():
     """
     Prints a list of standard import functions than can be quickly copy pasted for use.
     """
 
+    # ---------------- #
+    # Common Libraries #
+    # ---------------- #
+
     print("""
-            # standardized modules
-            import seaborn as sns
-            import pandas as pd
-            import numpy as np
-            import os
-            import matplotlib.pyplot as plt
-            import scipy.stats as stats
-            from sklearn.model_selection import train_test_split
-            
+    
+    # Standard Imports
+    import os
+    import requests
+    import numpy as np
+    import pandas as pd
+
+    # Working with Dates & Times
+    from sklearn.model_selection import TimeSeriesSplit
+    from datetime import timedelta, datetime
+
+    # Working with Math & Stats
+    import statsmodels.api as sm
+    import scipy.stats as stats
+
+    # to evaluate performance using rmse
+    from sklearn.metrics import mean_squared_error
+    from math import sqrt 
+
+    # holt's linear trend model. 
+    from statsmodels.tsa.api import Holt
+
+    # Plots, Graphs, & Visualization
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import StrMethodFormatter
+    from matplotlib.dates import DateFormatter
+
+    # plotting defaults
+    plt.rc('figure', figsize=(13, 7))
+    plt.style.use('seaborn-whitegrid')
+    plt.rc('font', size=16)
+
+    """)
+
+    # --------- #
+    # Data Sets #
+    # --------- #
+
+    from pydataset import data
+    # Call from the vega_datasets library like so:
+        ## df = data.
+
+    from vega_datasets import data
+    # Call from the vega_datasets library like so:
+        ## df = data.sf_temps()
+
+
+    # -------------- #
+    # Action Imports #
+    # -------------- #
+
+    # Warnings 
+    import warnings 
+    warnings.filterwarnings("ignore")
+
+    # ------------ #
+    # JUPYTER ONLY #
+    # ------------ #
+    """
+    # Disable autosave
+    %autosave 0
+
+    # Increases Display Resolution for Graphs 
+    %matplotlib inline 
+    %config InlineBackend.figure_format = 'retina'
+
+    # Left Align Tables in Jupyter Notebook
+    from IPython.core.display import HTML
+    table_css = 'table {align:left;display:block}'
+    HTML('{}'.format(table_css))
+
+    """
+
+    # ------------- #
+    # Local Imports #
+    # ------------- #
+
+    # env containing sensitive access credentials
+    from env import user, password, host
+
+    # importing sys
+    import sys
+
+    # adding 00_helper_files to the system path
+    sys.path.insert(0, '/Users/qmcbt/codeup-data-science/00_helper_files')
+
+    # Import Helper Modules
+    import QMCBT_00_quicktips as qt
+    import QMCBT_01_acquire as acq
+    import QMCBT_02_prepare as prep
+    import QMCBT_03_explore as exp
+    import QMCBT_04_visualize as viz
+    import QMCBT_05_model as mod
+    import QMCBT_wrangle as w
+
+######################### Display Imports #########################
+
+
+    print("""
             # Decision Tree and Model Evaluation Imports
             from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text
             from sklearn.model_selection import train_test_split
             from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
-            
-            #my modules
-            import QMCBT_wrangle as w
-            import QMCBT_explore_evaluate as ee
-            import QMCBT_[00]quick_tips as tips
-            import QMCBT_[01]acquire as acquire
-            import QMCBT_[02]prepare as prepare
-            import QMCBT_[03]explore as explore
-            import QMCBT_[04]evaluate as evaluate""")
-
+            """)
     
     
 ######################### TABLE OF CONTENTS #########################
@@ -69,7 +157,7 @@ def tips():
     """    
     
     print('%who')
-    print('df.describe(include='all')')
+    print("df.describe(include='all')")
     print('')
     print('')
     print('')
@@ -97,22 +185,22 @@ def cs_confusion_matrix():
 
     print(' **POSITIVE (+)** = insert Positive statement here  ')
     print(' **NEGATIVE (-)** = insert Negative statement here  ')  
-    print(' 
+    print('') 
     print(' **RECALL**  ')  
     print(' TP / (TP + FN)  ')  
     print(' Use for less **Type II** errors when **FN** is worst outcome  ')  
     print(' Maximize for **RECALL** if Cost of **FN** > Cost of **FP**  ')  
-    print(' 
+    print('') 
     print(' **PRECISION**  ')  
     print(' TP / (TP + FP)  ')  
     print(' Use for less **Type I** errors when **FP** is worst outcome  ')  
     print(' Maximize for **PRECISION** if Cost of **FP** > Cost of **FN**  ')  
-    print(' 
+    print('') 
     print(' **ACCURACY**  ')  
     print(' (TP + TN)/(FP+FN+TP+TN)  ')  
-    print(' total     print(' prediction TRUE / total  ')  
+    print(' prediction TRUE / total  ')  
     print(' Maximize for **ACCURACY** if neither **RECALL** or **PRECISION** outweigh eachother  ')  
-    print(' ')
+    print('')
     print(' * **Classification Confusion Matrix** (actual_col, prediction_row)(Positive_first, Negative_second)  ')
     print('                       +------------------------------------------+  ')
     print('                       | actual Positive (+) | actual Negative(-) |  ')
@@ -121,7 +209,7 @@ def cs_confusion_matrix():
     print(' +---------------------+---------------------+--------------------+  ')
     print(' |  pred Negative (-)  |     FN (Type II)    |     TN             |  ')
     print(' +---------------------+---------------------+--------------------+  ')
-    print('  ')
+    print('')
     print(' * <b>sklearn Confusion Matrix</b> (prediction_col, actual_row)(Negative_first, Positive_second)  ')
     print('                       +--------------------------------------+  ')
     print('                       | pred Negative(-) | pred Positive (+) |  ')
@@ -130,21 +218,21 @@ def cs_confusion_matrix():
     print(' +---------------------+------------------+-------------------+  ')
     print(' | actual Positive (+) |   FN (Type II)   |         TP        |  ')  
     print(' +---------------------+------------------+-------------------+  ')
-    print('  ') 
+    print('') 
     print(' **FP**: We **predicted** it was a **POSITIVE** when it was **actually** a **NEGATIVE**  ')  
     print(' *    FP = We **FALSE**LY predicted it was **POSITIVE**  ')  
     print(' * False = Our prediction was False, it was actually the opposite of our prediction  ')  
     print(' * Oops... **TYPE I** error!  ')  
-    print('  ') 
+    print('') 
     print(' **FN**: We **predicted** it was a **NEGATIVE** when it was **actually** a **POSITIVE**  ')  
     print(' *    FN = We **FALSE**LY predicted it was **NEGATIVE**  ')  
     print(' * False = Our prediction was False, it was actually the opposite of our prediction  ')  
     print(' * Oops... **TYPE II** error!  ')  
-    print('  ') 
+    print('') 
     print(' **TP**: We **predicted** it was a **POSITIVE** and it was **actually** a **POSITIVE**  ')  
     print(' *   TP = We **TRUE**LY predicted it was **POSITIVE**  ')  
     print(' * True = Our prediction was True, it was actually the same as our prediction  ')  
-    print('  ') 
+    print('') 
     print(' **TN**: We **predicted** it was a **NEGATIVE** and it was **actually** a **NEGATIVE**  ')  
     print(' *   TN = We **TRUE**LY predicted it was **NEGATIVE**  ')  
     print(' * True = Our prediction was True, it was actually the same as our prediction  ') 
@@ -154,27 +242,27 @@ def cs_confusion_matrix():
 def cs_hypothesis():
 
     print(' **A. Set Hypothesis**  ')  
-    print('  ') 
+    print('') 
     print(' * One Tail (```<= | >```) or Two Tails (```== | !=```)?\  ')
     print('  **two_tail (gender, been_manager)**  ')  
-    print('  ') 
-    print('  ') 
+    print('') 
+    print('') 
     print(' * One Sample or Two Samples?\  ')
     print('  **two_sample (gender, been_manager)**  ')  
-    print('  ') 
-    print('  ') 
+    print('') 
+    print('') 
     print(' * Continuous or Discreat?\  ')
     print('  **Discreat (gender) vs Discreat (been_manager) = $Chi^2$**  ')  
     print('      * T-Test = ```Discreat``` vs ```Continuous```  ')
     print('      * Pearson’s = ```Continuous``` vs ```Continuous``` (linear)  ')
     print('      * $Chi^2$ = ```Discreat``` vs ```Discreat```  ')
-    print('  ') 
-    print('  ') 
+    print('') 
+    print('') 
     print(' * $𝐻_0$: The opposite of what I am trying to prove\  ')  
     print('  **$H_{0}$: The employee gender is **NOT** ```dependent``` on whether the employee has been a manager**\  ')
     print('  ```employees.gender ``` != ```employees.been_manager```  ')  
-    print('  ') 
-    print('  ') 
+    print('') 
+    print('') 
     print(' * $𝐻_𝑎$: What am I trying to prove\  ')  
     print('  **$H_{a}$: The employee gender is ```dependent``` on whether the employee has been a manager**\  ')  
     print('  ```employees.gender ``` == ```employees.been_manager```  ')
